ac4fc9a0bafb741bde248732827e08468e5ca1f0
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2018-12-27 20:26:44 -0200

    modals and view more

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,23 +1,41 @@
 <template>
-    <div :id="id" :class="['modal',active?'is-active':'']">
-        <div class="modal-background">{{background}}</div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">{{title}}</p>
-                <button class="delete" aria-label="close"></button>
-            </header>
-            <section class="modal-card-body">
-                {{body}}
-            </section>
-            <footer class="modal-card-foot">
-                <button class="button is-success">Save changes</button>
-                <button class="button">Cancel</button>
-            </footer>
+    <transition name="bounce"> 
+        <div v-show="active" :id="id" :class="['modal','is-active']">
+            <div class="modal-background">{{background}}</div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">{{title}}</p>
+                    <button class="delete" aria-label="close" @click="OpenClose()"></button>
+                </header>
+                <section class="modal-card-body">
+                    {{body}}
+                </section>
+                
+            </div>
         </div>
-    </div>
+    </transition>
 </template>
 <script>
 export default {
-    props:['id','active','background','title','body']
+    props:['id','background','title','body'],
+    created(){
+        this.$root.$on(this.id+'-open',()=>{
+            this.OpenClose();
+        });
+        this.$root.$on(this.id+'-close',()=>{
+             this.OpenClose();
+        });
+    
+    },
+    data(){
+        return{
+            active:false
+        }
+    },
+    methods:{
+        OpenClose(){
+            this.active = !this.active;
+        }
+    }
 }
 </script>
--- pages/About.vue
+++ pages/About.vue
@@ -53,52 +53,99 @@
                         <ul class="info-list">
                             <li>
                                 <strong>HTML5 + css3</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <i class="fa fa-info-circle" data-target="htemlcss-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="75" max="100">75%</progress>
                             </li>
                             <li>
                                 <strong>SEO</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <i class="fa fa-info-circle" data-target="seo-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="70" max="100">70%</progress>
                             </li>
                             <li>
                                 <strong>Javascript</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <i class="fa fa-info-circle" data-target="js-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="67" max="100">67%</progress>
                             </li>
                              <li>
-                                <strong>VueJS</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <strong>VueJS/NuxtJS</strong>
+                                <i class="fa fa-info-circle" data-target="vuenuxt-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="80" max="100">80%</progress>
                             </li>
                             <li>
                                 <strong>PHP</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <i class="fa fa-info-circle" data-target="php-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="85" max="100">85%</progress>
                             </li>
                             <li>
                                 <strong>Laravel</strong>
-                                <i class="fa fa-info-circle"></i>
+                                <i class="fa fa-info-circle" data-target="laravel-modal" @click="openModal($event)"></i>
                                 <progress class="progress is-small" value="50" max="100">50%</progress>
                             </li>
                             <li>
                                 <strong>Codeigniter</strong>
-                                <i class="fa fa-info-circle modal-button" data-target="ci-modal" aria-haspopup="true" ></i>
+                                <i class="fa fa-info-circle modal-button" data-target="ci-modal" @click="openModal($event)" aria-haspopup="true" ></i>
                                 <progress class="progress is-small" value="90" max="100">90%</progress>
                             </li>
-                            <li class="arrow-down">
-                                <div>
+                            <li class="arrow-down" v-show="!showExtras">
+                                <div @click="LoadOrUnloadAbilities()">
                                     <strong>Ver Mais</strong>
-                                    <i class="fa fa-arrow-down" aria-hidden="true"></i>
+                                    <i :class="['fa', showExtrasClicked?'fa-spinner':'fa-arrow-down']" aria-hidden="true"></i>
                                 </div>
-                                
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
+            <transition name="fade">
+            <div id="extra-abilities" class="info-list" v-show="showExtras">
+                <div class="columns">
+                    
+                    <div class="column">
+                        <strong>Bootstrap</strong>
+                        <i class="fa fa-info-circle" data-target="bootstrap-modal" @click="openModal($event)"></i>
+                        <progress class="progress is-small" value="90" max="100">90%</progress>
+                    </div>   
+                    <div class="column">
+                        <strong>MySQL</strong>
+                        <i class="fa fa-info-circle" data-target="mysql-modal" @click="openModal($event)"></i>
+                        <progress class="progress is-small" value="90" max="100">90%</progress>
+                    </div> 
+                </div>
+                <div class="columns">
+                    <div class="column">
+                        <strong>Bulma</strong>
+                        <i class="fa fa-info-circle" data-target="bulma-modal" @click="openModal($event)"></i>
+                        <progress class="progress is-small" value="65" max="100">65%</progress>
+                    </div>
+                    <div class="column">
+                        <strong>Web Services</strong>
+                        <i class="fa fa-info-circle" data-target="ws-modal" @click="openModal($event)"></i>
+                        <progress class="progress is-small" value="70" max="100">70%</progress>
+                    </div>    
+                    
+                </div>
+                <div class="columns">
+                    <div class="column arrow-down" style="margin-top:1.5rem;" v-show="showExtras">
+                        <div @click="LoadOrUnloadAbilities()">
+                            <i :class="['fa', showExtrasClicked?'fa-spinner':'fa-arrow-up']" aria-hidden="true"></i>
+                            <strong style="margin-top:-0.8rem;">Ver Menos</strong>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </transition>
         </div>
-        <cd-modal :id="'ci-modal'" :background="''" :title="'Codeigniter'" :body="'teste'" :active="''"/>
+        <cd-modal :id="'htemlcss-modal'" :background="''" :title="'HTML e CSS3'" :body="'teste'"/>
+        <cd-modal :id="'seo-modal'" :background="''" :title="'SEO'" :body="'teste'"/>
+        <cd-modal :id="'js-modal'" :background="''" :title="'Javascript'" :body="'teste'"/>
+        <cd-modal :id="'vuenuxt-modal'" :background="''" :title="'VueJS e NuxtJS'" :body="'teste'"/>
+        <cd-modal :id="'php-modal'" :background="''" :title="'PHP'" :body="'teste'"/>
+        <cd-modal :id="'laravel-modal'" :background="''" :title="'Laravel'" :body="'teste'"/>
+        <cd-modal :id="'bootstrap-modal'" :background="''" :title="'Bootstrap'" :body="'teste'"/>
+        <cd-modal :id="'mysql-modal'" :background="''" :title="'MySQL'" :body="'teste'"/>
+        <cd-modal :id="'bulma-modal'" :background="''" :title="'Bulma'" :body="'teste'"/>
+        <cd-modal :id="'ws-modal'" :background="''" :title="'Web Services'" :body="'teste'"/>
     </section>
 </template>
 <script>
@@ -106,10 +153,26 @@ import Modal from '~/components/Modal.vue';
 export default {
     components:{
         'cd-modal': Modal
+    },
+    data(){
+        return{
+            showExtras: false,
+            showExtrasClicked:false
+        }
+    },
+    methods:{
+        LoadOrUnloadAbilities(){
+            this.showExtrasClicked = true;
+            setTimeout(() => { this.showExtras = !this.showExtras;this.showExtrasClicked = false; }, 1000);
+        },
+        openModal(event){
+           this.$root.$emit(event.target.dataset.target+'-open'); 
+        }
     }
 }
 </script>
 <style scoped>
+
 .arrow-down{
     text-align: center;
     width: 100%;
@@ -166,14 +229,14 @@ export default {
 	padding: 0 0 1.5rem 0;
 	margin-bottom: .6rem;
 }
-#full-profile .info-list li a{
+#full-profile .info-list a{
     font-family: "poppins-bold", sans-serif;
     text-transform: uppercase;
     font-size: 1.0rem;
     font-weight: 600;
     padding: 1.5rem;
 }
-#full-profile .info-list li strong {
+#full-profile .info-list strong {
 	font-family: "poppins-bold", sans-serif;
 	color: #313131;
 	text-transform: uppercase;
@@ -181,7 +244,7 @@ export default {
 	font-size: 1.0rem;
 	line-height: 3rem;
 }
-#full-profile .info-list li span {
+#full-profile .info-list span {
 	display: block;
 	font-family: "poppins-regular", sans-serif;
 	color: #888888;
@@ -193,4 +256,16 @@ export default {
     border-radius: 0;
 }
     
+@-webkit-keyframes rotation {
+    from {
+            -webkit-transform: rotate(0deg);
+    }
+    to {
+            -webkit-transform: rotate(359deg);
+    }
+}
+.fa-spinner{
+    -webkit-animation: rotation 1s infinite linear;
+}
+
 </style>
