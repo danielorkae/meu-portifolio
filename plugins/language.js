e0f96ecda7f5c0da1e457ba8ff4cf637a4f87995
Mateus Cordeiro <mateuscordeiro@rits.com.br>
2019-06-13 21:33:07 -0300

    dist branch

--- plugins/language.js
+++
@@ -1,6 +0,0 @@
-export default ({ app }) => {
-    // Get localized path for homepage
-    // Get path to switch current route to French
-    const switchLocalePath = (lang_code)=>{app.i18n.switchLocalePath(lang_code)}
-    
-}
